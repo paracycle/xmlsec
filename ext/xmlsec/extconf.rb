b1667cdd51ced5ca105f61806691c2270ab46e7c
Ufuk Kayserilioglu <ufuk@enkuba.com>
2012-06-14 00:14:35 +0300

    Trying yet again

--- ext/xmlsec/extconf.rb
+++ ext/xmlsec/extconf.rb
@@ -1,19 +1,19 @@
 require 'mkmf'
 
-unless pkg_config('xmlsec1-openssl')
+#unless pkg_config('xmlsec1-openssl')
   root = File.expand_path('../../..', __FILE__)
 
   Dir.chdir(root) do
     system "git submodule init"
     system "git submodule update"
   end
-  
+
   Dir.chdir(File.join(root, 'vendor/xmlsec')) do
     system "./configure", "--prefix=#{root}"
     system "make"
     system "make install"
     system "make clean"
   end
-end
+#end
 dir_config('opt', "#{root}/include", "#{root}/lib")
 create_makefile('xmlsec/xmlsec_ext')

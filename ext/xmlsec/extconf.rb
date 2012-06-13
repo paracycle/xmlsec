9f1d3e3e6b05a2600833b89599dd90864f029270
Ufuk Kayserilioglu <ufuk@enkuba.com>
2012-06-14 01:02:35 +0300

    Let this be the last one...

--- ext/xmlsec/extconf.rb
+++ ext/xmlsec/extconf.rb
@@ -12,12 +12,13 @@ require 'mkmf'
 
   Dir.chdir(File.join(root, 'vendor/xmlsec')) do
     puts("Building vendor xmlsec")
-    system "./autogen.sh", "--prefix=#{root} --without-libxslt"
+    system "./autogen.sh", "--prefix=#{root}", "--without-libxslt"
     #system "./configure", "--prefix=#{root}"
     system "make"
     system "make install"
     system "make clean"
   end
 #end
-dir_config('opt', "#{root}/include/xmlsec1", "#{root}/lib")
+dir_config('xmlsec', "#{root}/include/xmlsec1", "#{root}/lib")
+dir_config('libxml2', "/usr/include/libxml2", "/usr/lib")
 create_makefile('xmlsec/xmlsec_ext')
